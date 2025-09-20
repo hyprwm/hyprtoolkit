3532c2ed88762ce4b9f84a710f5b3e4cedf2fa14
Vaxry <vaxry@vaxry.net>
2025-09-20 17:28:37 +0100

    palette: init

---
+++ include/hyprtoolkit/palette/Palette.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <hyprutils/memory/SharedPtr.hpp>
+#include <hyprgraphics/color/Color.hpp>
+
+namespace Hyprtoolkit {
+    class CToolkitColor {
+      public:
+        CToolkitColor() = default;
+        CToolkitColor(const Hyprgraphics::CColor& color, float a = 1.F);
+        ~CToolkitColor();
+
+        Hyprgraphics::CColor m_color = Hyprgraphics::CColor::SSRGB{.r = 0.F, .g = 0.F, .b = 0.F};
+        float                m_a     = 1.F;
+    };
+
+    class CPalette {
+      public:
+        ~CPalette() = default;
+
+        /* Get the best palette possible. Retrieves configured system palette if available, default
+            otherwise */
+        static Hyprutils::Memory::CSharedPointer<CPalette> palette();
+
+        /* Default fallback palette */
+        static Hyprutils::Memory::CSharedPointer<CPalette> defaultPalette();
+        /* Empty palette with just black */
+        static Hyprutils::Memory::CSharedPointer<CPalette> emptyPalette();
+        /* Config palette if exists */
+        static Hyprutils::Memory::CSharedPointer<CPalette> configPalette();
+
+        struct {
+            CToolkitColor background;
+            CToolkitColor text;
+            CToolkitColor base;
+            CToolkitColor alternateBase;
+            CToolkitColor brightText;
+            CToolkitColor accent;
+            CToolkitColor accentSecondary;
+        } m_colors;
+
+      private:
+        CPalette() = default;
+    }
+};
---
+++ src/palette/Palette.cpp
@@ -0,0 +1,38 @@
+#include <hyprtoolkit/palette/Palette.hpp>
+
+#include "../helpers/Memory.hpp"
+
+using namespace Hyprtoolkit;
+
+CToolkitColor::CToolkitColor(const Hyprgraphics::CColor& color, float a) : m_color(color), m_a(a) {
+    ;
+}
+
+SP<CPalette> CPalette::palette() {
+    auto CONFIG = configPalette();
+    if (!CONFIG)
+        return defaultPalette();
+    return CONFIG;
+}
+
+SP<CPalette> CPalette::defaultPalette() {
+    auto palette = makeShared<CPalette>();
+
+    palette->m_colors.background      = {Hyprgraphics::CColor::SSRGB{.r = 24 / 255.F, .g = 24 / 255.F, .b = 24 / 255.F}, 1.F};
+    palette->m_colors.base            = {Hyprgraphics::CColor::SSRGB{.r = 32 / 255.F, .g = 32 / 255.F, .b = 32 / 255.F}, 1.F};
+    palette->m_colors.text            = {Hyprgraphics::CColor::SSRGB{.r = 221 / 255.F, .g = 221 / 255.F, .b = 221 / 255.F}, 1.F};
+    palette->m_colors.alternateBase   = {Hyprgraphics::CColor::SSRGB{.r = 39 / 255.F, .g = 39 / 255.F, .b = 39 / 255.F}, 1.F};
+    palette->m_colors.brightText      = {Hyprgraphics::CColor::SSRGB{.r = 255 / 255.F, .g = 221 / 255.F, .b = 221 / 255.F}, 1.F};
+    palette->m_colors.accent          = {Hyprgraphics::CColor::SSRGB{.r = 0 / 255.F, .g = 225 / 255.F, .b = 210 / 255.F}, 1.F};
+    palette->m_colors.accentSecondary = {Hyprgraphics::CColor::SSRGB{.r = 0 / 255.F, .g = 164 / 255.F, .b = 247 / 255.F}, 1.F};
+
+    return palette;
+}
+
+SP<CPalette> CPalette::emptyPalette() {
+    return makeShared<CPalette>();
+}
+
+SP<CPalette> CPalette::configPalette() {
+    return nullptr; // FIXME:
+}
