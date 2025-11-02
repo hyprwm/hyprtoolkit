94761922f3f153367a228b2036745d15f87ef3e6
Vaxry <vaxry@vaxry.net>
2025-11-02 13:48:24 +0000

    e

--- src/helpers/UTF8.cpp
+++ src/helpers/UTF8.cpp
@@ -115,6 +115,8 @@ TEST(UTF8, Length) {
 
 TEST(UTF8, Substr) {
     EXPECT_EQ(UTF8::substr("", 0, 0), "");
+    EXPECT_EQ(UTF8::substr("死", 1), "");
+    EXPECT_EQ(UTF8::substr("死", 0, 0), "");
     EXPECT_EQ(UTF8::substr("Hello", 0, 3), "Hel");
     EXPECT_EQ(UTF8::substr("世界", 1, 1), "界");
     EXPECT_EQ(UTF8::substr("世界is酷薄", 1), "界is酷薄");
@@ -123,6 +125,7 @@ TEST(UTF8, Substr) {
 
 TEST(UTF8, UTF8ToOffset) {
     EXPECT_EQ(UTF8::utf8ToOffset("", 0), 0);
+    EXPECT_EQ(UTF8::utf8ToOffset("Hello", 20000), 5);
     EXPECT_EQ(UTF8::utf8ToOffset("Hello", 3), 3);
     EXPECT_EQ(UTF8::utf8ToOffset("魑魅魍魎", 3), 9);
     EXPECT_EQ(UTF8::utf8ToOffset("a魑魅魍魎", 3), 7);
