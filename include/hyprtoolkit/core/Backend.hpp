3d298fbfec1899b28450df0e4c5f3caff85edbaf
Vaxry <vaxry@vaxry.net>
2025-11-24 14:33:29 +0000

    stuf

--- include/hyprtoolkit/core/Backend.hpp
+++ include/hyprtoolkit/core/Backend.hpp
@@ -95,11 +95,6 @@ namespace Hyprtoolkit {
         */
         virtual std::expected<Hyprutils::Memory::CSharedPointer<ISessionLockState>, eSessionLockError> aquireSessionLock() = 0;
 
-        /*
-            Add a log connection for a HU CLogger.
-        */
-        virtual void setLogConnection(Hyprutils::CLI::CLoggerConnection&& conn) = 0;
-
         struct {
             /*
                 Get notified when a new output was added.
