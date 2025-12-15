05591fc98910adb8c0a5322587043e34bf88e7bf
Vaxry <vaxry@vaxry.net>
2025-12-15 16:13:57 +0000

    tests: add text test

---
+++ tests/unit/elements/Text.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include <element/text/Text.hpp>
+#include <hyprtoolkit/core/Backend.hpp>
+
+using namespace Hyprtoolkit;
+
+TEST(Element, text) {
+    auto backend = IBackend::create();
+
+    auto text = CTextBuilder::begin()->text(R"(Hello <a href="https://hypr.land">link</a>! Hi <a href="https://hypr.land">link2</a>!)")->commence();
+
+    EXPECT_EQ(text->m_impl->parsedText, "Hello <u><span foreground=\"#4eecf8ff\">link</span></u>! Hi <u><span foreground=\"#4eecf8ff\">link2</span></u>!");
+}
