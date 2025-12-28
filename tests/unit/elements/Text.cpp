3072f0cf6b8e56fa4d295f507a212384735c04c0
Vaxry <vaxry@vaxry.net>
2025-12-28 19:36:08 +0100

    tests: fix test runs on CI runners

--- tests/unit/elements/Text.cpp
+++ tests/unit/elements/Text.cpp
@@ -3,15 +3,16 @@
 #include <element/text/Text.hpp>
 #include <hyprtoolkit/core/Backend.hpp>
 
+#include "../tricks/Tricks.hpp"
+
 using namespace Hyprtoolkit;
 
 TEST(Element, text) {
-    auto backend = IBackend::create();
+    Tests::Tricks::createBackendSupport();
 
     auto text = CTextBuilder::begin()->text(R"(Hello <a href="https://hypr.land">link</a>! Hi <a href="https://hypr.land">link2</a>!)")->commence();
 
     EXPECT_EQ(text->m_impl->parsedText, "Hello <u><span foreground=\"#4eecf8ff\">link</span></u>! Hi <u><span foreground=\"#4eecf8ff\">link2</span></u>!");
 
     text.reset();
-    backend->destroy();
 }
