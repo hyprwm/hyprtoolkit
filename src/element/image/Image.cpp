8ad5606e9d8612111d7f29c2f89fe8f043b07a01
j4kuuu <j4kuuu>
2026-06-19 13:54:26 +0200

    element/image: include the fit mode in the cache key
    the cached texture bakes in the fit mode, but the cache key was just the path
    (plus the svg/icon size), so two image elements sharing a path with different
    fit modes collided on one cache entry and the second drew with the first's fit.
    shows up with hyprpaper putting one image on two monitors with different fit_mode.

--- src/element/image/Image.cpp
+++ src/element/image/Image.cpp
@@ -170,9 +170,11 @@ std::string SImageImpl::getCacheString() {
         return std::format("data-{:x}", rc<uintptr_t>(data.data.data()));
 
     if (!data.icon)
-        return data.path.ends_with(".svg") ? std::format("{}-{}x{}", data.path, preferredSvgSize().x, preferredSvgSize().y) : data.path;
+        return data.path.ends_with(".svg") ? std::format("{}-{}x{}-{}", data.path, preferredSvgSize().x, preferredSvgSize().y, sc<int>(data.fitMode)) :
+                                             std::format("{}-{}", data.path, sc<int>(data.fitMode));
     else
-        return std::format("icon-{}-{}x{}", reinterpretPointerCast<CSystemIconDescription>(data.icon)->m_bestPath, preferredSvgSize().x, preferredSvgSize().y);
+        return std::format("icon-{}-{}x{}-{}", reinterpretPointerCast<CSystemIconDescription>(data.icon)->m_bestPath, preferredSvgSize().x, preferredSvgSize().y,
+                           sc<int>(data.fitMode));
 }
 
 SP<CImageBuilder> CImageElement::rebuild() {
